65506c50e145ec4c16c68e3e7efb61d97f82c706
shanerodrigues <shane2010r@hotmail.com>
2022-01-12 13:36:03 +1100

    updated legend

--- src/entities/LegendItems.js
+++ src/entities/LegendItems.js
@@ -1,23 +1,23 @@
 import LegendItem from "./LegendItem"
 
 const legendItems = [
-  new LegendItem("50,000 +", "#2e436b", (cases) => cases > 50_000, "white"),
+  new LegendItem("150,000 +", "#2e436b", (cases) => cases > 150_000, "white"),
   new LegendItem(
-    "10,000 - 50,000",
+    "50,000 - 150,000",
     "#4565a1",
-    (cases) => cases > 10_000 && cases <= 50_000,
+    (cases) => cases > 50_000 && cases <= 150_000,
     "white"
   ),
   new LegendItem(
-    "1,000 - 10,000",
+    "15,000 - 50,000",
     "#829bc9",
-    (cases) => (cases > 1_000) & (cases <= 10_000),
+    (cases) => (cases > 15_000) & (cases <= 50_000),
     "black"
   ),
   new LegendItem(
-    "0 - 1,000",
+    "1000 - 15,000",
     "#c9d4e8",
-    (cases) => (cases >= 0) & (cases <= 1_000),
+    (cases) => (cases >= 1000) & (cases <= 15_000),
     "black"
   ),
 ]
